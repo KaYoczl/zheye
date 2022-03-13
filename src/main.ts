1b01691fd122ba65db583c49df9e72253a033d91
KaYo <kayoczl@163.com>
2022-03-14 01:08:42 +0800

    7-4 axios 的基本用法和独家后端API 使用（必看）

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { createApp } from 'vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
 import App from './App.vue'
 
+axios.defaults.baseURL = 'http://api.vikingship.xyz/api'
+// axios.interceptors.request.use(config => {
+//   config.params = { ...config.params, icode: 'C6A6C4086133360B' }
+//   return config
+// })
+axios.get('/columns').then(res => {
+  console.log(res)
+})
 const app = createApp(App)
 app.use(router)
 app.use(store)
